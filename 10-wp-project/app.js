65cd44af66a2b0030666bf2c156dc65be831b24e
ElioShemEngr <122136358+ElioShemEngr@users.noreply.github.com>
2025-01-31 20:03:45 -0500

    create 10-wp-project

---
+++ 10-wp-project/app.js
@@ -0,0 +1,4 @@
+const toppingsNaranja = document.querySelectorAll('.topping.fondo-naranja');
+console.log(toppingsNaranja[0]);
+console.log(toppingsNaranja[1]);
+console.log(toppingsNaranja.length);
