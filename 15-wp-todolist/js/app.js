0e6033481a57af5111b83577901862d5daf7867f
ElioShemEngr <122136358+ElioShemEngr@users.noreply.github.com>
2025-02-20 18:35:34 -0500

    create todo-list project web

---
+++ 15-wp-todolist/js/app.js
@@ -0,0 +1,6 @@
+const botonCrear = document.getElementById('boton');
+const contenedorTareas = document.getElementsByClassName('contenedor-tareas');
+
+botonCrear.addEventListener('click', function() {
+    window.alert('Has presionado el boton crear');
+});
